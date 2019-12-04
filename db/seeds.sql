1c155b31a192b8a1312eafef199ffde44a8dba2b
steven cain <stevencainn@gmail.com>
2019-12-03 21:41:53 -0500

    made sequal schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Plain and simple burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Quarter Pounder with cheese");
+
+INSERT INTO burgers (burger_name) VALUES ("Diablo Burger");
