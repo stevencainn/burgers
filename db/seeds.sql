f6212c7d3a39b89b872dc378784560d2941eddbd
steven cain <stevencainn@gmail.com>
2019-12-10 20:20:22 -0500

    connections

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,8 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
-
-INSERT INTO burgers (burger_name) VALUES ("Plain and simple burger");
-
-INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
-
-INSERT INTO burgers (burger_name) VALUES ("Quarter Pounder with cheese");
-
-INSERT INTO burgers (burger_name) VALUES ("Diablo Burger");
+INSERT INTO burgers (burger_name) VALUES 
+("Cheeseburger"),
+("Plain and simple burger"),
+("Breakfast Burger"),
+("Quarter Pounder with cheese"),
+("Diablo Burger");
